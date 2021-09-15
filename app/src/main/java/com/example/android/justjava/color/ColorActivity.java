ab8f3c5b9ad3a32289af871ab82db8184d329427
Trang90 <laraolivia13@gmail.com>
2021-09-15 20:26:19 +0200

    request audio focus

--- app/src/main/java/com/example/android/justjava/color/ColorActivity.java
+++ app/src/main/java/com/example/android/justjava/color/ColorActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.justjava.color;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -25,12 +27,43 @@ public class ColorActivity extends AppCompatActivity {
     private ActivityNumberBinding binding;
     private MediaPlayer mMediaPlayer;
 
+    private AudioManager mAudioManager;
+
+
+
+    private MediaPlayer.OnCompletionListener mOnCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            mMediaPlayer.release();
+
+        }
+    };
+
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK && mMediaPlayer != null) {
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);} else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+                mMediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+                // Stop playback and clean up resources
+                releaseMediaPlayer();
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityNumberBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
 
         final ArrayList<Word> colors = new ArrayList<>();
         colors.add(new Word(R.drawable.color_red,"red", "rot", R.raw.color_red));
@@ -48,13 +81,45 @@ public class ColorActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Word color= colors.get(position);
-                mMediaPlayer = MediaPlayer.create(ColorActivity.this, color.getMediaPlayerId());
-                mMediaPlayer.start();
+                releaseMediaPlayer();
+
+                //gọi lại audio bị ngừng
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC,
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+//we have audio focus now
+
+                    //ko cần dòng này nữa
+                //mMediaPlayer = MediaPlayer.create(ColorActivity.this, color.getMediaPlayerId());
+                    if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                        mMediaPlayer = MediaPlayer.create(ColorActivity.this, color.getMediaPlayerId());
+
+                        // Start the audio file
+                        mMediaPlayer.start();
+
+                        // Setup a listener on the media player, so that we can stop and release the
+                        // media player once the sound has finished playing.
+                        mMediaPlayer.setOnCompletionListener(mOnCompletionListener);
+                    }
+
             }
+
         });
 
 
     }
 
+    @Override
+    protected void onStop(){
+        super.onStop();
+        releaseMediaPlayer();
+    }
 
+private void releaseMediaPlayer(){
+        if (mMediaPlayer!=null){
+            mMediaPlayer.release();
+            mMediaPlayer= null;
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+}
 }
--- app/src/main/java/com/example/android/justjava/family/FamilyActivity.java
+++ app/src/main/java/com/example/android/justjava/family/FamilyActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.justjava.family;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
@@ -20,9 +22,42 @@ import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
-
     private ActivityNumberBinding binding;
     private MediaPlayer mMediaPlayer;
+
+    private MediaPlayer.OnCompletionListener mOnCompletionListener= new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            mMediaPlayer.release();
+        }
+    };
+
+    private AudioManager mAudioManager;
+
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+                // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+                // our app is allowed to continue playing sound but at a lower volume. We'll treat
+                // both cases the same way because our app is playing short sound files.
+
+                // Pause playback and reset player to the start of the file. That way, we can
+                // play the word from the beginning when we resume playback.
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+                mMediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+                // Stop playback and clean up resources
+                releaseMediaPlayer();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +65,8 @@ public class FamilyActivity extends AppCompatActivity {
 
         binding = ActivityNumberBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
         final ArrayList<Word> family =new ArrayList<>();
         family.add(new Word(R.drawable.family_father,"father", "vater", R.raw.family_father));
@@ -51,13 +88,41 @@ public class FamilyActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Word families = family.get(position);
-                mMediaPlayer= MediaPlayer.create(FamilyActivity.this, families.getMediaPlayerId());
-                mMediaPlayer.start();
+                releaseMediaPlayer();
+
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // We have audio focus now.
+                    mMediaPlayer = MediaPlayer.create(FamilyActivity.this, families.getMediaPlayerId());
+
+                    // Start the audio file
+                    mMediaPlayer.start();
+
+                    // Setup a listener on the media player, so that we can stop and release the
+                    // media player once the sound has finished playing.
+                    mMediaPlayer.setOnCompletionListener(mOnCompletionListener);
+                }
+
 
             }
         });
 
     }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        releaseMediaPlayer();
+    }
 
+    private void releaseMediaPlayer(){
+        if (mMediaPlayer !=null){
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+
+    }
 
 }
